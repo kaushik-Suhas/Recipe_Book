c456a0da3e256e530c74f723c99fd4d452567148
kaushikSuhas94 <kaushiksuhas94@gmail.com>
2021-06-29 22:45:55 +0530

    Observable

--- recipe-book/src/app/shopping-list/shopping-list.component.ts
+++ recipe-book/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shoppinglist.service';
 
@@ -7,19 +8,23 @@ import { ShoppingListService } from './shoppinglist.service';
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
+  private igChabgeSub: Subscription;
 
   constructor(private slService: ShoppingListService) { }
 
   ngOnInit(): void {
     this.ingredients = this.slService.getIngredient() 
-    this.slService.ingredientsChanged
+    this.igChabgeSub = this.slService.ingredientsChanged
     .subscribe(
-      (ingredients: Ingredient[]) => {
+        (ingredients: Ingredient[]) => {
         this.ingredients = ingredients;
       }
     );
   }
-
+  
+  ngOnDestroy() {
+    this.igChabgeSub.unsubscribe();
+  }
 }
